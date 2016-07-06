1cf4e1be78dc1cbb5cc01e3ad6c15f5796c3e999
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-07-06 16:51:52 +0200

    Implement normalisation of latest posts widget

---
+++ web/modules/custom/dbcdk_community_content/src/Normalizer/Widget/LatestPostsWidgetNormalizer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\dbcdk_community_content\Normalizer\Widget;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\dbcdk_community_content\FieldNormalizer\IntegerFieldNormalizer;
+use Drupal\dbcdk_community_content\FieldNormalizer\StringItemFieldNormalizer;
+
+/**
+ * Normalizer for the latests posts widget.
+ */
+class LatestPostsWidgetNormalizer extends WidgetNormalizer {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getSupportedBundle() {
+    return 'latest_posts';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getWidgetName() {
+    return 'LatestsPostsWidget';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getWidgetConfig(FieldableEntityInterface $object) {
+    $data = [
+      'displayTitle' => (new StringItemFieldNormalizer())->normalize($object->get('field_title')->first()),
+      'postsToLoad' => (new IntegerFieldNormalizer())->normalize($object->get('field_num_items')->first()),
+    ];
+    $group_field = $object->get('field_community_service_group');
+    if (!$group_field->isEmpty()) {
+      $data['groupId'] = (new IntegerFieldNormalizer())->normalize($group_field->first());
+    }
+
+    return $data;
+  }
+
+}
