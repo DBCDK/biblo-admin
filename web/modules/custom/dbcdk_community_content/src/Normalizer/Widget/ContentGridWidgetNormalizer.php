c3b307b0e7bb2a6137ae7c800bb3e0bdc8649e41
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-08-17 12:46:28 +0200

    Implement new content grid normalizer
    - Add new content grid field which again contanins a content grid item
    field. Content grid items has all fields supported by the widget.
    - Implement new normaliser to support normalisation of content grid
    fields.

---
+++ web/modules/custom/dbcdk_community_content/src/Normalizer/Widget/ContentGridWidgetNormalizer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\dbcdk_community_content\Normalizer\Widget;
+
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\dbcdk_community_content\FieldNormalizer\FileFieldNormalizer;
+use Drupal\dbcdk_community_content\FieldNormalizer\StringItemFieldNormalizer;
+use Drupal\dbcdk_community_content\FieldNormalizer\UrlFieldNormalizer;
+
+/**
+ * Normalizer for content grid widget.
+ *
+ * @see https://github.com/DBCDK/biblo/tree/master/src/client/components/WidgetContainer/widgets/ContentGridWidget
+ */
+class ContentGridWidgetNormalizer extends DefaultWidgetNormalizer {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getSupportedBundle() {
+    return 'content_grid';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getWidgetName() {
+    return 'ContentGridWidget';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getWidgetConfig(FieldableEntityInterface $object) {
+    $config = [
+      'items' => [],
+    ];
+
+    // A content grid consists of multiple items each with their own
+    // configuration. This maps to a field where each value corresponds to an
+    // item.
+    $id = 1;
+    foreach ($object->get('field_content_grid_items') as $grid_item_reference) {
+      /* @var \Drupal\Core\Field\FieldItemInterface $grid_item_reference */
+      $grid_item = $grid_item_reference->get('entity')->getValue();
+
+      // Each item supports the default properties so we can use the parent
+      // method for each item even though it is meant for the entire widget.
+      $item_config = parent::getWidgetConfig($grid_item);
+
+      // Set a numeric id for each item.
+      $item_config['id'] = $id++;
+
+      // Add optional widget specific configuration based on field values.
+      $text_field = $grid_item->get('field_text');
+      if (!$text_field->isEmpty()) {
+        $item_config['text'] = (new StringItemFieldNormalizer())->normalize($text_field->first());
+      }
+
+      $image_field = $grid_item->get('field_image');
+      if (!$image_field->isEmpty()) {
+        $item_config['imageUrl'] = (new FileFieldNormalizer($this->fileStorage))->normalize($image_field->first());
+      }
+
+      $link_field = $grid_item->get('field_link');
+      if (!$link_field->isEmpty()) {
+        $item_config['url'] = (new UrlFieldNormalizer())->normalize($link_field->first());
+      }
+
+      $config['items'][] = $item_config;
+    }
+
+    return $config;
+  }
+
+}
