5dbd38afd2cc882fbc759c160db4855822cc90b6
Søren Vibjerg <vibjerg@gmail.com>
2016-10-11 14:08:44 +0200

    Keep group id when editing widget

--- web/modules/custom/dbcdk_community_reference_field/src/Plugin/Field/FieldWidget/GroupReferenceAutocomplete.php
+++ web/modules/custom/dbcdk_community_reference_field/src/Plugin/Field/FieldWidget/GroupReferenceAutocomplete.php
@@ -39,7 +39,7 @@ class GroupReferenceAutocomplete extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element += [
+    $element['value'] = $element + [
       '#type' => 'dbcdk_community_group_reference_autocomplete',
       '#default_value' => $items[$delta]->value,
     ];
