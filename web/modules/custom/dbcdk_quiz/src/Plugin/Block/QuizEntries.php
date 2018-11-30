20b64cf746b8d1aefb6b616058e63c2fd2405d56
Soeren Vibjerg <vibjerg@gmail.com>
2018-11-30 15:32:37 +0100

    use custom webpack config instead of create-react-app

---
+++ web/modules/custom/dbcdk_quiz/src/Plugin/Block/QuizEntries.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\dbcdk_quiz\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'CampaignListBlock' block.
+ *
+ * @Block(
+ *  id = "dbcdk_quiz_admin_block",
+ *  admin_label = @Translation("Quiz Admin"),
+ * )
+ */
+class QuizEntries extends BlockBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        $token = \Drupal::service('config.factory')->get('dbcdk_openplatform.settings')->get('smaug_token');
+        return [
+            '#markup' => '<div id="quiz-admin">Dette er en placeholder til biblo quiz admin</div>',
+            '#attached' => array(
+                'library' => array('dbcdk_quiz/dbcdk_quiz'),
+                'drupalSettings' => ['openPlatformToken' => $token],
+            ),
+        ];
+    }
+
+}
